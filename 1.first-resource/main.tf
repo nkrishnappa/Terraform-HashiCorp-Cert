cf72b2f1d2ae4f3895c7488cc1930cf28b87ed88
nkrishnappa <nkrishnappa@rbbn.com>
2021-07-08 12:58:43 +0000

    resource

---
+++ 1.first-resource/main.tf
@@ -0,0 +1,12 @@
+# File :
+# 1.first-resouce/main.tf
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
