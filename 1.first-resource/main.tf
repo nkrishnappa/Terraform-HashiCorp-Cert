49884ad90823d64bafb232198f18e4ea3244d87a
nkrishnappa <nkrishnappa@rbbn.com>
2021-07-09 07:36:04 +0000

    variables and Dynamic

--- 1.first-resource/main.tf
+++ 1.first-resource/main.tf
@@ -9,4 +9,17 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+ 
+  tags = {
+    # string interpolation - .12 not needed
+    /* Name = ${var.vpcname} */
+    Name = var.vpcname
+  }
 }
+
+/* 
+var.vpcname
+var.mylist[0]
+var.mymap["Key1"]
+var.inputname 
+*/
